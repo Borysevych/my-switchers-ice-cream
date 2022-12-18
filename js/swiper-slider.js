d332fa3a2a670f52493f747c347c8290631107f6
Borysevych <borysevych.rus@gmail.com>
2022-12-18 14:11:04 +0200

    my section Final version
    Fast final version

--- js/swiper-slider.js
+++ js/swiper-slider.js
@@ -3,14 +3,14 @@ const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
+  // pagination
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
 
 
-  // And if we need scrollbar
+  // scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
